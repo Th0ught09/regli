3c052b9ed4ca477d47d8b751f4befd5c90ab965d
Kirk Matt <kirkmatt@proton.me>
2025-12-19 12:51:39 +0000

    removed print statemtent accidentally left for debugging

--- src/io_util.rs
+++ src/io_util.rs
@@ -4,7 +4,6 @@ pub fn read_file(files: &Vec<String>) -> String {
     let mut all_text: String = String::new();
     let file_iter = files.iter();
     for val in file_iter {
-        println!("{}", val);
         let read_text = get_file_contents(val);
         all_text = all_text + &read_text;
     }
