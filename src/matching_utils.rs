fc7e85ee532fce456aece1fe42b10beea25a383c
Kirk Matt <kirkmatt@proton.me>
2026-01-14 19:08:05 +0000

    modularised files

--- src/matching_utils.rs
+++ src/matching_utils.rs
@@ -1,6 +1,11 @@
 use regex::Regex;
 
-pub fn updated_matches(message: &String,  matches:&mut Vec<String>,  non_matches:&mut Vec<String>, messages: Vec<String>) {
+pub fn updated_matches(
+    message: &String,
+    matches: &mut Vec<String>,
+    non_matches: &mut Vec<String>,
+    messages: Vec<String>,
+) {
     let re = Regex::new(message.as_str()).unwrap();
     for message in messages {
         if re.is_match(message.as_str()) {
