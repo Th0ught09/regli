56e563288fd1f26b4371ade611fcd04c61655795
Kirk Matt <kirkmatt@proton.me>
2026-01-28 12:35:00 +0000

    initial templates for shell code

--- src/shell_utils.rs
+++ src/shell_utils.rs
@@ -1,3 +1,9 @@
-pub fn start_shell_search(){
+use std::{env,fs};
 
+pub fn start_shell_search(){
+    println!("{}",env::current_dir().unwrap().display());
+    let paths = fs::read_dir(env::current_dir().unwrap()).unwrap();
+    for path in paths{
+        println!("{}", path.unwrap().path().display());
+    }
 }
