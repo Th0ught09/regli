24d75b0c6b58d9cd39fad9554dfd192f046be413
Kirk Matt <kirkmatt@proton.me>
2026-02-01 09:32:34 +0000

    custom functions for testing if string is directory or file

--- src/shell_utils.rs
+++ src/shell_utils.rs
@@ -1,4 +1,4 @@
-use std::{env, fs};
+use std::{env, fs, path::Path};
 
 pub fn start_shell_search() -> Vec<String> {
     let mut files = Vec::new();
@@ -7,4 +7,12 @@ pub fn start_shell_search() -> Vec<String> {
         files.push(path.unwrap().path().display().to_string());
     }
     files
+}
+
+pub fn is_path_file(path: String) -> bool {
+    Path::new(path.as_str()).is_file()
+}
+
+pub fn is_path_dir(path: String) -> bool {
+    Path::new(path.as_str()).is_dir()
 }
