b493752289a704d89069ec0095e08040edddef7e
Kirk Matt <kirkmatt@proton.me>
2026-03-13 16:57:16 +0000

    added extra deault extensions

--- src/const_utils.rs
+++ src/const_utils.rs
@@ -1,3 +1,9 @@
 pub fn get_default_extensions() -> Vec<String> {
-    vec![String::from("txt")]
+    vec![
+        String::from("txt"),
+        String::from("md"),
+        String::from("org"),
+        String::from("csv"),
+        String::from("tsv"),
+    ]
 }
