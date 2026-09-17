907aa20ff727769cf64b24198d17d670c52acef6
Kirk Matt <kirkmatt@proton.me>
2026-09-17 09:59:28 +0100

    depreciated function, test not needed

--- src/matching_utils.rs
+++ src/matching_utils.rs
@@ -17,24 +17,7 @@ use regex::Regex;
 //     }
 // }
 
-#[allow(dead_code)]
-fn test_matches() -> (Vec<String>, Vec<String>) {
-    let message = "h".to_string();
-    let mut matches: Vec<String> = Vec::new();
-    let mut non_matches: Vec<String> = Vec::new();
-    let messages = vec![String::from("hi"), String::from("gq")];
-    // update_matches(&message, &mut matches, &mut non_matches, messages);
-    (matches, non_matches)
-}
-
 #[cfg(test)]
 mod tests {
     use super::*;
-
-    #[test]
-    fn pushes_match() {
-        let (matches, non_matches) = test_matches();
-        assert_eq!(matches, vec!["hi"]);
-        assert_eq!(non_matches, vec!["gq"]);
-    }
 }
